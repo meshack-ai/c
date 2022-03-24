1aaba21b39a79e9a6de000998200866f3a0fcef7
meshack <kamasingoi@gmail.com>
2022-03-24 20:01:41 +0300

    mehack c

---
+++ thur24.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+    int main(){
+         int num;
+        printf("Hello enter a number\n");
+        scanf("%d", &num);
+        printf(" your number %d", num);
+
+        return 0;
+    }
